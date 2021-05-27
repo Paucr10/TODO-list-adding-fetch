bcd8908b9b791ed17269708985c70917f4d9668c
Paulo Jiménez <pauloajimenezm76@outlook.com>
2021-05-27 16:54:51 +0000

    update

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,91 +1,231 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 export function Home() {
-	const FetchUrl =
-		"https://assets.breatheco.de/apis/fake/todos/user/usuarioPaucr10";
-	const [currentItem, setCurrentItem] = useState();
-	const [todos, setTodos] = useState([]);
+	//creamos las constantes que manejaran la app con su usestate
+	const [Searched, setSearched] = useState(false);
+	const [inputSearch, SetSearchValue] = useState("");
+	const [inputValue, SetInputValue] = useState("");
+	const [todoList, settodoList] = useState([]);
 
-	const onChangeHandler = e => {
-		setCurrentItem(e.target.value);
-	};
+	//La URL se pedira al inicio para ir a buscar el usuario correcto dentro de todos los usuarios que tiene el fake api de la academia
+	let URL = "https://assets.breatheco.de/apis/fake/todos/user/" + inputSearch;
 
-	const addTodo = async e => {
-		if (e.key == "Enter" && e.target.value !== "") {
-			const NuevoTodo = { label: currentItem, done: false };
-			// Se hace el put
-			await fetch(FetchUrl, {
-				method: "PUT",
-				body: JSON.stringify([...todos, NuevoTodo]),
-				headers: { "Content-Type": "application/json" }
+	async function CreateUserProfile() {
+		let myHeaders = new Headers();
+		myHeaders.append("Content-Type", "application/json");
+
+		let raw = JSON.stringify([]);
+
+		let requestOptions = {
+			method: "POST",
+			headers: myHeaders,
+			body: raw,
+			redirect: "follow"
+		};
+
+		const response = await fetch(URL, requestOptions)
+			.then(res => {
+				if (res.status == 200) {
+					SearchTodotodoList();
+				}
 			})
-				.then(res => res.json())
-				.then(data => console.log([data]));
-			// Se hace el get para obtener el todo
-			await fetch(FetchUrl)
-				.then(res => res.json())
-				.then(data => {
-					setTodos(data);
-					setCurrentItem("");
-				});
-		}
-	};
+			.then(result => console.log(result))
+			.catch(error => console.log("error", error));
+	}
 
-	const delTodo = async key => {
-		// Como me quedaba un todo, lo fuerzo a que me quede la lista en blanco
-		if (todos.length === 1) {
-			setTodos([]);
-		} else {
-			const listaNueva = todos.filter((item, index) => {
-				return key !== index ? item : null;
+	async function SearchTodotodoList() {
+		let requestOptions = {
+			method: "GET",
+			redirect: "follow"
+		};
+
+		const response = await fetch(URL, requestOptions)
+			.then(res => {
+				if (res.status == 404) {
+					CreateUserProfile();
+				} else {
+					return res.json();
+				}
+			})
+			.catch(error => console.log("error", error));
+
+		settodoList(response);
+		setSearched(true);
+		console.log(response);
+	}
+
+	async function UpdateTodo(newarray) {
+		console.log(newarray);
+		let myHeaders = new Headers();
+		myHeaders.append("Content-Type", "application/json");
+
+		let raw = JSON.stringify(newarray);
+
+		let requestOptions = {
+			method: "PUT",
+			headers: myHeaders,
+			body: raw,
+			redirect: "follow"
+		};
+
+		const response = await fetch(URL, requestOptions)
+			.then(res => {
+				if (res.status == 200) {
+					SearchTodotodoList();
+				}
+			})
+			.catch(error => console.log("error", error));
+	}
+
+	async function EraseTodo() {
+		let requestOptions = {
+			method: "DELETE",
+			redirect: "follow"
+		};
+
+		const response = await fetch(URL, requestOptions)
+			.then(res => {
+				if (res.status == 200) {
+					setSearched(false);
+					SetInputValue("");
+					return res;
+				}
+			})
+			.catch(error => console.log("error", error));
+
+		SetInputValue("");
+		console.log(response);
+	}
+
+	const Addtodo = e => {
+		if (e.key === "Enter") {
+			const newtodoList = todoList.concat({
+				label: inputValue,
+				done: false
 			});
-			// Se hace le put
-			await fetch(FetchUrl, {
-				method: "PUT",
-				body: JSON.stringify(listaNueva),
-				headers: { "Content-Type": "application/json" }
-			})
-				.then(res => res.json())
-				.then(data => console.log([data]));
-			// Se hace el get para eliminar un todo
-			await fetch(FetchUrl)
-				.then(res => res.json())
-				.then(data => {
-					setTodos(data);
-					// e.target.value = " ";
-				});
+			UpdateTodo(newtodoList);
+			SetInputValue("");
 		}
 	};
 
-	return (
-		<div className="principal">
-			<h1>Things ToDo</h1>
-			<h3>Please type your tasks</h3>
-			<header className="home-header">
-				<div className="wrapper">
-					<div className="Input-wrapper">
-						<input
-							value={currentItem}
-							onKeyPress={addTodo}
-							onChange={onChangeHandler}
-							tye="text"
-							placeholder="What needs to be done?"
-						/>
-						<div>
-							{todos.map((todo, key) => {
-								return (
-									<div className="window" key={key}>
-										{todo.label}
-										<button onClick={() => delTodo(key)}>
-											x
-										</button>
-									</div>
-								);
-							})}
+	function RemoveTodo(e) {
+		const newtodoList = todoList.filter(item => item !== e);
+		//console.log(newtodoList.length);
+		if (newtodoList.length == 0) {
+			EraseTodo();
+		} else {
+			UpdateTodo(newtodoList);
+		}
+	}
+
+	const TodoList = () => {
+		if (todoList == undefined) {
+			return (
+				<div>
+					<p>Hello, Add something to your todo list</p>
+				</div>
+			);
+		} else {
+			return (
+				<div>
+					<ul className="todoList-group">
+						{todoList.map((item, id) => (
+							<li
+								className="todoList-group-item d-flex justify-content-between align-items-center"
+								key={id}>
+								{item.label}
+								<button
+									onClick={() => RemoveTodo(item)}
+									className="btn btn-link">
+									<i className="fas fa-times"></i>
+								</button>
+							</li>
+						))}
+					</ul>
+					<span className="float-left text-muted mt-2">
+						{todoList.length} left
+					</span>
+				</div>
+			);
+		}
+	};
+
+	if (!Searched) {
+		return (
+			<div className="text-white text-center mt-5 justify-content-center.bg-light">
+				<h1>Things ToDo</h1>
+				<div className="card cardtodos mx-auto">
+					<div className="card-body">
+						<div className="input-group mb-3">
+							<input
+								type="text"
+								value={inputSearch}
+								className="form-control"
+								onChange={e => SetSearchValue(e.target.value)}
+								placeholder="UserName"
+							/>
+							<div className="input-group-append ml-3">
+								<button
+									type="button"
+									className="btn btn-success"
+									onClick={() => SearchTodotodoList()}>
+									Go
+								</button>
+							</div>
 						</div>
 					</div>
 				</div>
-			</header>
-		</div>
-	);
+			</div>
+		);
+	} else {
+		return (
+			<div className="text-center mt-5 justify-content-center.bg-light">
+				<h1 className="text-white">Things ToDo </h1>
+				<div className="card cardtodos mx-auto">
+					<div className="card-body">
+						<div className="input-group mb-3">
+							<input
+								type="text"
+								value={inputSearch}
+								className="form-control"
+								onChange={e => SetSearchValue(e.target.value)}
+								placeholder="UserName"
+							/>
+							<div className="input-group-append">
+								<button
+									type="button"
+									className="btn btn-primary"
+									onClick={() => SearchTodotodoList()}>
+									Go
+								</button>
+							</div>
+						</div>
+						<div>
+							<div className="input-group mb-3">
+								<input
+									type="text"
+									className="form-control"
+									placeholder="Something you wanna do?"
+									value={inputValue}
+									onChange={e =>
+										SetInputValue(e.target.value)
+									}
+									onKeyUp={Addtodo}
+								/>
+							</div>
+							<TodoList />
+						</div>
+					</div>
+					<div className="card-footer">
+						<button
+							type="button"
+							className="btn btn-success"
+							onClick={() => EraseTodo()}>
+							Clear All
+						</button>
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
